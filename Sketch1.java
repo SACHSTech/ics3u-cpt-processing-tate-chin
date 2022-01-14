b71d158c6fb9c61ab76982f1d41aec720595178c
Ajuaman <tate.chin24@gmail.com>
2022-01-14 09:21:33 -0500

    implemented smoother movement

--- Sketch1.java
+++ Sketch1.java
@@ -9,7 +9,7 @@ public class Sketch1 extends PApplet {
   float speed = 6;
   float gravity = 5;
 
-  boolean movingRight = false;
+  boolean keyLeft, keyRight;
 
   public void settings() {
     size(600, 600);
@@ -23,6 +23,7 @@ public class Sketch1 extends PApplet {
   public void draw() {
     background(201, 222, 255);
 
+    changePosition();
     // DRAWING PLATFORMS
     rect(100, 100, 100, 10);
     rect(300, 150, 100, 10);
@@ -33,26 +34,50 @@ public class Sketch1 extends PApplet {
     image(player, playerX, playerY, width/7, height/7);
     
     // GRAVITY
-    playerY+=gravity;
     
+    // COLLISIONS WITH PLATFORMS
+
+    // DRAWING SPIKES
+    triangle(0, 600, 100, 550, 125, 600);
+    triangle(125, 600, 200, 550, 250, 600);
+    triangle(250, 600, 300, 550, 370, 600);
+    triangle(375, 600, 400, 550, 450, 600);
+    triangle(470, 600, 500, 550, 600, 600);
+
+    // IF PLAYER HITS A SPIKE THEN IT TRIGGERS A GAME OVER SCREEN
+    if(playerY > 500){
+      background(0);
+      text("Game Over", 300, 300);
+      text("Restart The Screen", 300, 400);
+      
+    }
+  }
+
+  void changePosition() {
+    if(keyLeft) playerX-=speed;
+    if(keyRight) playerX+=speed;
   }
 
   @Override
   public void keyPressed() {
-      
+  
       if(keyCode == LEFT){
-        playerX -= speed;
+        keyLeft = true;
       }
       else if(keyCode == RIGHT){
-        playerX += speed;
-        movingRight = true;
+        keyRight = true;
       }
       super.keyPressed();
   }
   @Override
   public void keyReleased() {
 
-      movingRight = false;
+    if (keyCode == LEFT) {
+      keyLeft = false;
+    }
+    else if (keyCode == RIGHT) {
+      keyRight = false;
+    }
       super.keyReleased();
   }
 }
