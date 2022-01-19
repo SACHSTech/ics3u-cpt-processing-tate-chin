82ba1d3da6e1b28c3c38d0067636ef89fcd25904
Angel Lin <angel@Angels-MacBook-Pro.local>
2022-01-19 17:48:34 -0500

    Changed platforms and added doors to the other screens

--- Sketch2.java
+++ Sketch2.java
@@ -4,9 +4,9 @@ import processing.core.PImage;
 public class Sketch2 extends PApplet {
 	
   PImage player;
-  float playerX = 300;
-  float playerY = 70;
-  float speed = 6;
+  float playerX = 275;
+  float playerY = 450;
+  float speed = 4;
   float gravity = 3;
   
   boolean keyLeft, keyRight;
@@ -25,14 +25,46 @@ public class Sketch2 extends PApplet {
   public void draw() {
     background(201, 222, 255);
     changePosition();
-
-    stroke(0);
-    fill(225);
     
     // DRAWING PLAYER
     image(player, playerX, playerY, width/12, height/12);
     
-    //rect(275,  )
+    stroke(0);
+    fill(225);
+
+    rect(275, 500, 50, 5);
+    rect(350, 450, 50, 5);
+    rect(455, 420, 50, 5);
+    rect(455, 340, 50, 5);
+    rect(400, 270, 50, 5);
+    rect(500, 230, 50, 5);
+    rect(380, 170, 50, 5);
+    rect(300, 110, 50, 5);
+    rect(400, 70, 50, 5);
+    rect(550, 500, 50, 5);
+
+    rect(200, 100, 15, 500);
+
+    rect(185, 100, 50, 5);
+    rect(100, 30, 50, 5);
+    rect(0, 75, 50, 5);
+    rect(20, 130, 50, 5);
+    rect(50, 180, 50, 5);
+    rect(140, 240, 50, 5);
+    rect(40, 290, 50, 5);
+    rect(70, 350, 50, 5);
+    rect(120, 420, 50, 5);
+    rect(80, 480, 50, 5);
+    
+
+    rect(550, 75, 50, 5);
+    rect(0, 500, 50, 5);
+
+    stroke(138, 56, 33);
+    fill(138, 56, 33);
+
+    rect(575, 25, 25, 50);
+    rect(0, 450, 25, 50);
 
     /*
     rect(100, 100, 100, 10);
@@ -43,41 +75,100 @@ public class Sketch2 extends PApplet {
     rect(300, 500, 100, 10);
     */
 
-    if(playerX < 0){
+    if(playerX < 0 && playerY < 550 && playerY > 400){
       isOnLeftSide = true;
       isOnRightSide = false;
       isOnMiddleSide = false;
       background(254, 207, 255);
 
-      rect(100, 100, 100, 10);
-      rect(300, 150, 100, 10);
-      rect(200, 300, 100, 10);
-      rect(150, 400, 100, 10);
-      rect(450, 300, 100, 10);
-      rect(300, 500, 100, 10);
+      stroke(0);
+      fill(225);
+  
+      rect(275, 500, 50, 5);
+      rect(350, 450, 50, 5);
+      rect(455, 420, 50, 5);
+      rect(455, 340, 50, 5);
+      rect(400, 270, 50, 5);
+      rect(500, 230, 50, 5);
+      rect(380, 170, 50, 5);
+      rect(300, 110, 50, 5);
+      rect(400, 70, 50, 5);
+      rect(550, 500, 50, 5);
+  
+      rect(200, 100, 15, 500);
+  
+      rect(185, 100, 50, 5);
+      rect(100, 30, 50, 5);
+      rect(0, 75, 50, 5);
+      rect(20, 130, 50, 5);
+      rect(50, 180, 50, 5);
+      rect(140, 240, 50, 5);
+      rect(40, 290, 50, 5);
+      rect(70, 350, 50, 5);
+      rect(120, 420, 50, 5);
+      rect(80, 480, 50, 5);
+      
+  
+      rect(550, 75, 50, 5);
+      rect(0, 500, 50, 5);
+  
+      stroke(138, 56, 33);
+      fill(138, 56, 33);
+  
+      rect(575, 450, 25, 50);
 
       image(player, playerX + 600, playerY, width/12, height/12);
 
     }
-    else if(playerX > width){
+    else if(playerX > width && playerY > 0 && playerY < 100){
       isOnRightSide = true;
       isOnLeftSide = false;
       isOnMiddleSide = false;
       background(208, 255, 163);
 
-      rect(100, 100, 100, 10);
-      rect(300, 150, 100, 10);
-      rect(200, 300, 100, 10);
-      rect(150, 400, 100, 10);
-      rect(450, 300, 100, 10);
-      rect(300, 500, 100, 10);
+      stroke(0);
+      fill(225);
+  
+      rect(275, 500, 50, 5);
+      rect(350, 450, 50, 5);
+      rect(455, 420, 50, 5);
+      rect(455, 340, 50, 5);
+      rect(400, 270, 50, 5);
+      rect(500, 230, 50, 5);
+      rect(380, 170, 50, 5);
+      rect(300, 110, 50, 5);
+      rect(400, 70, 50, 5);
+      rect(550, 500, 50, 5);
+  
+      rect(200, 100, 15, 500);
+  
+      rect(185, 100, 50, 5);
+      rect(100, 30, 50, 5);
+      rect(0, 75, 50, 5);
+      rect(20, 130, 50, 5);
+      rect(50, 180, 50, 5);
+      rect(140, 240, 50, 5);
+      rect(40, 290, 50, 5);
+      rect(70, 350, 50, 5);
+      rect(120, 420, 50, 5);
+      rect(80, 480, 50, 5);
+      
+  
+      rect(550, 75, 50, 5);
+      rect(0, 500, 50, 5);
+  
+      stroke(138, 56, 33);
+      fill(138, 56, 33);
+  
+      rect(0, 25, 25, 50);
 
       image(player, playerX - 600, playerY, width/12, height/12);
   
     }
     
     // GRAVITY
-    //playerY+=3;
+    // playerY+=3;
+
     // COLLISIONS WITH PLATFORMS
 
     // DRAWING SPIKES
