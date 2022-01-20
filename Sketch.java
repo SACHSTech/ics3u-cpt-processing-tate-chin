8c66411c871b23785bfde82efe1206423ed45ec9
Ajuaman <tate.chin24@gmail.com>
2022-01-19 20:08:37 -0500

    Added Rain and Decreased Jump height

--- Sketch.java
+++ Sketch.java
@@ -4,13 +4,15 @@ import processing.core.PImage;
 public class Sketch extends PApplet {
 	
   PImage player;
-  float playerX = 300;
-  float playerY = 70;
-  float speed = 6;
+  float playerX = 275;
+  float playerY = 450;
+  float speed = 4;
   float gravity = 3;
-
+  
   boolean keyLeft, keyRight;
-
+  boolean isOnLeftSide = false;
+  boolean isOnRightSide = false;
+  boolean isOnMiddleSide = true;
   public void settings() {
     size(600, 600);
   }
@@ -22,25 +24,151 @@ public class Sketch extends PApplet {
 
   public void draw() {
     background(201, 222, 255);
-
     changePosition();
-    // DRAWING PLATFORMS
-
+    
+    // DRAWING PLAYER
+    image(player, playerX, playerY, width/12, height/12);
+    
     stroke(0);
     fill(225);
+
+    rect(275, 500, 50, 5);
+    rect(350, 450, 50, 5);
+    rect(455, 420, 50, 5);
+    rect(455, 340, 50, 5);
+    rect(400, 270, 50, 5);
+    rect(500, 230, 50, 5);
+    rect(380, 170, 50, 5);
+    rect(300, 110, 50, 5);
+    rect(400, 70, 50, 5);
+    rect(550, 500, 50, 5);
+
+    rect(200, 100, 15, 500);
+
+    rect(185, 100, 50, 5);
+    rect(100, 30, 50, 5);
+    rect(0, 75, 50, 5);
+    rect(20, 130, 50, 5);
+    rect(50, 180, 50, 5);
+    rect(140, 240, 50, 5);
+    rect(40, 290, 50, 5);
+    rect(70, 350, 50, 5);
+    rect(120, 420, 50, 5);
+    rect(80, 480, 50, 5);
     
+
+    rect(550, 75, 50, 5);
+    rect(0, 500, 50, 5);
+
+    stroke(138, 56, 33);
+    fill(138, 56, 33);
+
+    rect(575, 25, 25, 50);
+    rect(0, 450, 25, 50);
+
+    /*
     rect(100, 100, 100, 10);
     rect(300, 150, 100, 10);
     rect(200, 300, 100, 10);
     rect(150, 400, 100, 10);
     rect(450, 300, 100, 10);
     rect(300, 500, 100, 10);
-    
-    // DRAWING PLAYER
-    image(player, playerX, playerY, width/7, height/7);
+    */
+
+    if(playerX < 0 && playerY < 550 && playerY > 400){
+      isOnLeftSide = true;
+      isOnRightSide = false;
+      isOnMiddleSide = false;
+      background(254, 207, 255);
+
+      stroke(0);
+      fill(225);
+  
+      rect(275, 500, 50, 5);
+      rect(350, 450, 50, 5);
+      rect(455, 420, 50, 5);
+      rect(455, 340, 50, 5);
+      rect(400, 270, 50, 5);
+      rect(500, 230, 50, 5);
+      rect(380, 170, 50, 5);
+      rect(300, 110, 50, 5);
+      rect(400, 70, 50, 5);
+      rect(550, 500, 50, 5);
+  
+      rect(200, 100, 15, 500);
+  
+      rect(185, 100, 50, 5);
+      rect(100, 30, 50, 5);
+      rect(0, 75, 50, 5);
+      rect(20, 130, 50, 5);
+      rect(50, 180, 50, 5);
+      rect(140, 240, 50, 5);
+      rect(40, 290, 50, 5);
+      rect(70, 350, 50, 5);
+      rect(120, 420, 50, 5);
+      rect(80, 480, 50, 5);
+      
+  
+      rect(550, 75, 50, 5);
+      rect(0, 500, 50, 5);
+  
+      stroke(138, 56, 33);
+      fill(138, 56, 33);
+  
+      rect(575, 450, 25, 50);
+
+      image(player, playerX + 600, playerY, width/12, height/12);
+
+    }
+    else if(playerX > width && playerY > 0 && playerY < 100){
+      isOnRightSide = true;
+      isOnLeftSide = false;
+      isOnMiddleSide = false;
+      background(208, 255, 163);
+
+      stroke(0);
+      fill(225);
+  
+      rect(275, 500, 50, 5);
+      rect(350, 450, 50, 5);
+      rect(455, 420, 50, 5);
+      rect(455, 340, 50, 5);
+      rect(400, 270, 50, 5);
+      rect(500, 230, 50, 5);
+      rect(380, 170, 50, 5);
+      rect(300, 110, 50, 5);
+      rect(400, 70, 50, 5);
+      rect(550, 500, 50, 5);
+  
+      rect(200, 100, 15, 500);
+  
+      rect(185, 100, 50, 5);
+      rect(100, 30, 50, 5);
+      rect(0, 75, 50, 5);
+      rect(20, 130, 50, 5);
+      rect(50, 180, 50, 5);
+      rect(140, 240, 50, 5);
+      rect(40, 290, 50, 5);
+      rect(70, 350, 50, 5);
+      rect(120, 420, 50, 5);
+      rect(80, 480, 50, 5);
+      
+  
+      rect(550, 75, 50, 5);
+      rect(0, 500, 50, 5);
+  
+      stroke(138, 56, 33);
+      fill(138, 56, 33);
+  
+      rect(0, 25, 25, 50);
+
+      image(player, playerX - 600, playerY, width/12, height/12);
+  
+    }
     
     // GRAVITY
-    
+    // playerY+=3;
+
     // COLLISIONS WITH PLATFORMS
 
     // DRAWING SPIKES
@@ -58,6 +186,9 @@ public class Sketch extends PApplet {
       text("Restart The Screen", 300, 400);
       
     }
+    // Circle Collections
+    stroke(0);
+    fill(50, 168, 82);
   }
 
   void changePosition() {
--- Sketch1.java
+++ Sketch1.java
@@ -4,15 +4,19 @@ import processing.core.PImage;
 public class Sketch1 extends PApplet {
 	
   PImage player;
-  float playerX = 300;
-  float playerY = 70;
-  float speed = 6;
+  float playerX = 275;
+  float playerY = 450;
+  float speed = 4;
   float gravity = 3;
-  
-  boolean keyLeft, keyRight;
+  float jumpHeight = 3;
+
+  boolean keyLeft, keyRight, keyUp;
   boolean isOnLeftSide = false;
   boolean isOnRightSide = false;
   boolean isOnMiddleSide = true;
+
+  float[] circleY = new float[25];
+
   public void settings() {
     size(600, 600);
   }
@@ -20,57 +24,182 @@ public class Sketch1 extends PApplet {
   public void setup() {
     // PLAYER
     player = loadImage("floppa.png");
+    for (int i = 0; i < circleY.length; i++) {
+      circleY[i] = random(height);
+    }
+
   }
 
   public void draw() {
     background(201, 222, 255);
     changePosition();
 
+    // DRAWING PLAYER
+    image(player, playerX, playerY, width/12, height/12);
+    
     stroke(0);
     fill(225);
-    
-    // DRAWING PLAYER
-    image(player, playerX, playerY, width/7, height/7);
-    
-    rect(100, 100, 100, 10);
-    rect(300, 150, 100, 10);
-    rect(200, 300, 100, 10);
-    rect(150, 400, 100, 10);
-    rect(450, 300, 100, 10);
-    rect(300, 500, 100, 10);
 
-    if(playerX < 0){
+    rect(275, 500, 50, 5);
+    rect(350, 450, 50, 5);
+    rect(455, 420, 50, 5);
+    rect(455, 340, 50, 5);
+    rect(400, 270, 50, 5);
+    rect(500, 230, 50, 5);
+    rect(380, 170, 50, 5);
+    rect(300, 110, 50, 5);
+    rect(400, 70, 50, 5);
+    rect(550, 500, 50, 5);
+
+    rect(200, 100, 15, 500);
+
+    rect(185, 100, 50, 5);
+    rect(100, 30, 50, 5);
+    rect(0, 75, 50, 5);
+    rect(20, 130, 50, 5);
+    rect(50, 180, 50, 5);
+    rect(140, 240, 50, 5);
+    rect(40, 290, 50, 5);
+    rect(70, 350, 50, 5);
+    rect(120, 420, 50, 5);
+    rect(80, 480, 50, 5);
+    
+
+    rect(550, 75, 50, 5);
+    rect(0, 500, 50, 5);
+
+    stroke(138, 56, 33);
+    fill(138, 56, 33);
+
+    rect(575, 25, 25, 50);
+    rect(0, 450, 25, 50);
+
+
+    if(playerX < 0 && playerY < 550 && playerY > 400){
       isOnLeftSide = true;
       isOnRightSide = false;
       isOnMiddleSide = false;
+      jumpHeight = 2;
       background(254, 207, 255);
 
-      rect(100, 100, 100, 10);
-      rect(300, 150, 100, 10);
-      rect(200, 300, 100, 10);
-      rect(150, 400, 100, 10);
-      rect(450, 300, 100, 10);
-      rect(300, 500, 100, 10);
+      // DRAWING RAIN
+      for (int i = 0; i < circleY.length; i++) {
+        float circleX = width * i / circleY.length;
+        ellipse(circleX, circleY[i], 5, 5);
+  
+        circleY[i]++;
+  
+        if (circleY[i] > height) {
+          circleY[i] = 0;
+        }
+      }
+
+      stroke(0);
+      fill(225);
+  
+      rect(275, 500, 50, 5);
+      rect(350, 450, 50, 5);
+      rect(455, 420, 50, 5);
+      rect(455, 340, 50, 5);
+      rect(400, 270, 50, 5);
+      rect(500, 230, 50, 5);
+      rect(380, 170, 50, 5);
+      rect(300, 110, 50, 5);
+      rect(400, 70, 50, 5);
+      rect(550, 500, 50, 5);
+  
+      rect(200, 100, 15, 500);
+  
+      rect(185, 100, 50, 5);
+      rect(100, 30, 50, 5);
+      rect(0, 75, 50, 5);
+      rect(20, 130, 50, 5);
+      rect(50, 180, 50, 5);
+      rect(140, 240, 50, 5);
+      rect(40, 290, 50, 5);
+      rect(70, 350, 50, 5);
+      rect(120, 420, 50, 5);
+      rect(80, 480, 50, 5);
+      
+  
+      rect(550, 75, 50, 5);
+      rect(0, 500, 50, 5);
+  
+      stroke(138, 56, 33);
+      fill(138, 56, 33);
+  
+      rect(575, 450, 25, 50);
+
+      image(player, playerX + 600, playerY, width/12, height/12);
 
     }
-    else if(playerX > width){
+
+    else if(playerX > width && playerY > 0 && playerY < 100){
       isOnRightSide = true;
       isOnLeftSide = false;
       isOnMiddleSide = false;
       background(208, 255, 163);
 
-      rect(100, 100, 100, 10);
-      rect(300, 150, 100, 10);
-      rect(200, 300, 100, 10);
-      rect(150, 400, 100, 10);
-      rect(450, 300, 100, 10);
-      rect(300, 500, 100, 10);
+      // DRAWING RAIN
+      jumpHeight = 2;
+      for (int i = 0; i < circleY.length; i++) {
+        float circleX = width * i / circleY.length;
+        ellipse(circleX, circleY[i], 5, 5);
+  
+        circleY[i]++;
+  
+        if (circleY[i] > height) {
+          circleY[i] = 0;
+        }
+      }
+
+      stroke(0);
+      fill(225);
+  
+      rect(275, 500, 50, 5);
+      rect(350, 450, 50, 5);
+      rect(455, 420, 50, 5);
+      rect(455, 340, 50, 5);
+      rect(400, 270, 50, 5);
+      rect(500, 230, 50, 5);
+      rect(380, 170, 50, 5);
+      rect(300, 110, 50, 5);
+      rect(400, 70, 50, 5);
+      rect(550, 500, 50, 5);
+  
+      rect(200, 100, 15, 500);
+  
+      rect(185, 100, 50, 5);
+      rect(100, 30, 50, 5);
+      rect(0, 75, 50, 5);
+      rect(20, 130, 50, 5);
+      rect(50, 180, 50, 5);
+      rect(140, 240, 50, 5);
+      rect(40, 290, 50, 5);
+      rect(70, 350, 50, 5);
+      rect(120, 420, 50, 5);
+      rect(80, 480, 50, 5);
       
+  
+      rect(550, 75, 50, 5);
+      rect(0, 500, 50, 5);
+  
+      stroke(138, 56, 33);
+      fill(138, 56, 33);
+  
+      rect(0, 25, 25, 50);
+
+      image(player, playerX - 600, playerY, width/12, height/12);
     }
     
     // GRAVITY
     //playerY+=3;
+
+
+
     // COLLISIONS WITH PLATFORMS
+
+
 
     // DRAWING SPIKES
     stroke(235, 52, 70);
@@ -95,6 +224,7 @@ public class Sketch1 extends PApplet {
   void changePosition() {
     if(keyLeft) playerX-=speed;
     if(keyRight) playerX+=speed;
+    if(keyUp) playerY -=speed;
   }
 
   @Override
@@ -106,7 +236,12 @@ public class Sketch1 extends PApplet {
       else if(keyCode == RIGHT){
         keyRight = true;
       }
+      
+      else if(keyCode == UP){
+        keyUp = true;
+      }
       super.keyPressed();
+
   }
   @Override
   public void keyReleased() {
@@ -116,6 +251,9 @@ public class Sketch1 extends PApplet {
     }
     else if (keyCode == RIGHT) {
       keyRight = false;
+    }
+    else if(keyCode == UP){
+      keyUp = false;
     }
       super.keyReleased();
   }
